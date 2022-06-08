f6f85db65ae5719d5ff1b0412d496483bf771009
TelevisionNinja <TelevisionNinja@gmail.com>
2022-06-08 13:00:31 -0500

    array slicing

--- src/arrayUtils.h
+++ src/arrayUtils.h
@@ -66,6 +66,7 @@ namespace tvnj {
 
     /**
      * returns a boolean for whether the sub vector is a subset of the main vector
+     * O(n + m)
      * 
      * @param {*} arr 
      * @param {*} subArr 
@@ -88,6 +89,7 @@ namespace tvnj {
 
     /**
      * returns a boolean for whether the sub vector is a subset of the main vector
+     * O(nm)
      * 
      * @param {*} arr 
      * @param {*} subArr 
@@ -112,6 +114,84 @@ namespace tvnj {
         }
 
         return true;
+    }
+
+    /**
+     * python like array slicing
+     * 
+     * @param str 
+     * @param start inclusive
+     * @param end exclusive by default
+     * @param step increment
+     * @param inclusive bool for making the parameter "end" inclusive
+     * @return a new vector
+     */
+    template <typename type>
+    std::vector<type> slice(std::vector<type> vector, int start, int end, int step = 1, bool inclusive = false) {
+        const int len = vector.size();
+
+        if (start < 0) {
+            start += len;
+        }
+
+        if (end < 0) {
+            end += len;
+        }
+
+        if (step == 0) {
+            step = 1;
+        }
+
+        std::vector<type> newVector;
+
+        if (step > 0) {
+            if (start <= end) {
+                if (start < 0) {
+                    start = 0;
+                }
+
+                if (end > len) {
+                    end = len - 1;
+                    inclusive = true;
+                }
+
+                int i = start;
+
+                while (i < end) {
+                    newVector.push_back(vector[i]);
+                    i += step;
+                }
+
+                if (inclusive && i == end) {
+                    newVector.push_back(vector[i]);
+                }
+            }
+        }
+        else {
+            if (end <= start) {
+                if (start >= len) {
+                    start = len - 1;
+                }
+
+                if (end < 0) {
+                    end = 0;
+                    inclusive = true;
+                }
+
+                int i = start;
+
+                while (i > end) {
+                    newVector.push_back(vector[i]);
+                    i += step;
+                }
+
+                if (inclusive && i == end) {
+                    newVector.push_back(vector[i]);
+                }
+            }
+        }
+
+        return newVector;
     }
 }
 
