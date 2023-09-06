95732167882aec169a63f5b691e3fa38e9661476
TelevisionNinja <TelevisionNinja@gmail.com>
2023-09-06 17:25:02 -0500

    added exponentiation with signed ints

--- src/mathUtils.cpp
+++ src/mathUtils.cpp
@@ -22,3 +22,35 @@ float tvnj::exponentiationBySquaring(float base, unsigned int power) {
 
     return result;
 }
+
+/**
+ * 
+ * @param base 
+ * @param power 
+ * @return float 
+ */
+float tvnj::exponentiationBySquaring(float base, int power) {
+    float result = 1;
+    float exponentiation = base;
+    bool isNegative = power < 0;
+    int i = power;
+
+    if (isNegative) {
+        i *= -1;
+    }
+
+    while (i != 0) { // i > 0
+        if (i % 2 != 0) {
+            result *= exponentiation;
+        }
+
+        exponentiation *= exponentiation;
+        i >>= 1;
+    }
+
+    if (isNegative) {
+        return 1 / result;
+    }
+
+    return result;
+}
--- src/mathUtils.h
+++ src/mathUtils.h
@@ -1,3 +1,4 @@
 namespace tvnj {
     float exponentiationBySquaring(float base, unsigned int power);
+    float exponentiationBySquaring(float base, int power);
 }
