0ec0a4bf63048b08e98d93baf49302419154ebdc
TelevisionNinja <TelevisionNinja@gmail.com>
2023-10-11 18:59:09 -0500

    added header guard

--- src/mathUtils.h
+++ src/mathUtils.h
@@ -1,4 +1,9 @@
+#ifndef STRINGUTILS_H
+#define STRINGUTILS_H
+
 namespace tvnj {
     float exponentiationBySquaring(float base, unsigned int power);
     float exponentiationBySquaring(float base, int power);
 }
+
+#endif
