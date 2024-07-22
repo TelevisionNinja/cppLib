bc0073c549835e60b8604e203a2aa0294af7fcc3
TelevisionNinja <TelevisionNinja@gmail.com>
2024-07-22 07:49:55 -0500

    added doubly linked list tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
 #include "heapTests.h"
+#include "doublyLinkedListTest.h"
 
 int main() {
     maxHeapTests();
+    doublyLinkedListTests();
+
+    std::cout << "Tests ended";
     return 0;
 }
