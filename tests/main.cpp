ea0698e667304bc57433902c112f456b07e05944
TelevisionNinja <TelevisionNinja@gmail.com>
2024-07-23 02:02:05 -0500

    added string tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+
 #include "heapTests.h"
 #include "doublyLinkedListTest.h"
+#include "stringUtilsTests.h"
 
 int main() {
     maxHeapTests();
     doublyLinkedListTests();
+    stringUtilsTests();
 
     std::cout << "Tests ended";
     return 0;
