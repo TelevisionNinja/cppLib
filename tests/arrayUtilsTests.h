981ff023c9198286279b0381fbe4408397586bbd
TelevisionNinja <TelevisionNinja@gmail.com>
2024-07-25 04:28:57 -0500

    added array slice tests

--- tests/arrayUtilsTests.h
+++ tests/arrayUtilsTests.h
@@ -1,5 +1,6 @@
 #include "../src/unitTest.h"
 #include "../src/arrayUtils.h"
+#include "../src/stringUtils.h"
 #include <vector>
 
 void arrayUtilsTests() {
@@ -21,4 +22,34 @@ void arrayUtilsTests() {
     UNIT_TEST_EQ(tvnj::indexOfNaiveSkipLast(testthree, testfive), 1);
     UNIT_TEST_EQ(tvnj::indexOfNaiveSkip(testfour, testfive), 2);
     UNIT_TEST_EQ(tvnj::indexOfNaiveSkipLast(testfour, testfive), 3);
+
+    std::vector<char> testing2 = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, 0), ""), "");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, 0, -1), ""), "");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, 0, 1, true), ""), "a");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, 0, -1, true), ""), "a");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, testing2.size()), ""), "abcdefg");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, -100000, testing2.size(), 1), ""), "abcdefg");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, 100000000), ""), "abcdefg");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, 100000000, 1, true), ""), "abcdefg");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), 0), ""), "");
+
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, -testing2.size(), testing2.size()), ""), "abcdefg");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 1, testing2.size()), ""), "bcdefg");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, testing2.size() - 1), ""), "abcdef");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 1, testing2.size() - 1), ""), "bcdef");
+
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), 0, -1), ""), "gfedcb");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), -10000000, -1), ""), "gfedcba");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, testing2.size(), -1), ""), "");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size()-1, 0, -1), ""), "gfedcb");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), 0, -1, true), ""), "gfedcba");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size()-1, 0, -1, true), ""), "gfedcba");
+
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, testing2.size(), 2), ""), "aceg");
+
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), 0, -2), ""), "gec");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), 0, -2, true), ""), "geca");
+    UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, testing2.size(), -2), ""), "");
 }
