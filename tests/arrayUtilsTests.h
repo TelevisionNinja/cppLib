be622eabf04a212e2dd8a7e1f938ad31c324541e
TelevisionNinja <TelevisionNinja@gmail.com>
2024-07-26 07:09:20 -0500

    added more array tests

--- tests/arrayUtilsTests.h
+++ tests/arrayUtilsTests.h
@@ -52,4 +52,86 @@ void arrayUtilsTests() {
     UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), 0, -2), ""), "gec");
     UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, testing2.size(), 0, -2, true), ""), "geca");
     UNIT_TEST_EQ(tvnj::join(tvnj::slice(testing2, 0, testing2.size(), -2), ""), "");
+
+    // binary search leftmost and right most tests
+    std::vector<int> array = {1,2,3,3,3,6,7,8,9,10}; // len = 10
+    int value = 3;
+    int left = 0;
+    int right = array.size() - 1;
+
+    UNIT_TEST_EQ(tvnj::binarySearchFirstSwappablePosition(array, value, left, right), 2);
+    UNIT_TEST_EQ(tvnj::binarySearchLastSwappablePosition(array, value, left, right), 4);
+
+    array = {1,2,3,4,5,6,7,7,7,10};
+    value = 7;
+    UNIT_TEST_EQ(tvnj::binarySearchFirstSwappablePosition(array, value, left, right), 6);
+    UNIT_TEST_EQ(tvnj::binarySearchLastSwappablePosition(array, value, left, right), 8);
+
+    array = {3,3,3,4,5,6,7,8,9,10};
+    value = 3;
+    UNIT_TEST_EQ(tvnj::binarySearchFirstSwappablePosition(array, value, left, right), 0);
+    UNIT_TEST_EQ(tvnj::binarySearchLastSwappablePosition(array, value, left, right), 2);
+
+    array = {1,2,3,4,5,6,7,8,8,8};
+    value = 8;
+    UNIT_TEST_EQ(tvnj::binarySearchFirstSwappablePosition(array, value, left, right), 7);
+    UNIT_TEST_EQ(tvnj::binarySearchLastSwappablePosition(array, value, left, right), 9);
+
+    array = {1,2,3,4,5,6,7,8,9,10};
+    value = 11;
+    UNIT_TEST_EQ(tvnj::binarySearchFirstSwappablePosition(array, value, left, right), 9);
+    UNIT_TEST_EQ(tvnj::binarySearchLastSwappablePosition(array, value, left, right), 9);
+
+    array = {1,2,3,4,5,6,7,8,9,10};
+    value = 0;
+    UNIT_TEST_EQ(tvnj::binarySearchFirstSwappablePosition(array, value, left, right), 0);
+    UNIT_TEST_EQ(tvnj::binarySearchLastSwappablePosition(array, value, left, right), 0);
+
+    // block swap tests
+    array = {1,2,3,4,5,6,7,8,9,10}; // length = 10
+    left = 0;
+    right = array.size() - 1;
+    tvnj::blockSwap(array, left, right);
+    UNIT_TEST_EQ(tvnj::join(array, ","), "6,7,8,9,10,1,2,3,4,5");
+
+    array = {1,2,3,4,5,6,7,8,9};
+    right = array.size() - 1;
+    tvnj::blockSwap(array, left, right);
+    UNIT_TEST_EQ(tvnj::join(array, ","), "6,7,8,9,5,1,2,3,4");
+
+    // block swap separated blocks
+    array = {1,2,3,4,5,6,7,8,9,10}; // length = 10
+    int start1 = 0;
+    int start2 = 5;
+    int end1 = 4;
+    tvnj::blockSwap(array, start1, end1, start2);
+    UNIT_TEST_EQ(tvnj::join(array, ","), "6,7,8,9,10,1,2,3,4,5");
+
+    array = {1,2,3,4,5,6,7,8,9,10};
+    start1 = 0;
+    start2 = 5;
+    end1 = 3;
+    tvnj::blockSwap(array, start1, end1, start2);
+    UNIT_TEST_EQ(tvnj::join(array, ","), "6,7,8,9,5,1,2,3,4,10");
+
+    array = {1,2,3,4,5,6,7,8,9,10};
+    start1 = 0;
+    start2 = 6;
+    end1 = 3;
+    tvnj::blockSwap(array, start1, end1, start2);
+    UNIT_TEST_EQ(tvnj::join(array, ","), "7,8,9,10,5,6,1,2,3,4");
+
+    array = {1,2,3,4,5,6,7,8,9};
+    start1 = 0;
+    start2 = 5;
+    end1 = 3;
+    tvnj::blockSwap(array, start1, end1, start2);
+    UNIT_TEST_EQ(tvnj::join(array, ","), "6,7,8,9,5,1,2,3,4");
+
+    array = {1,2,3,4,5,6,7,8,9};
+    start1 = 0;
+    start2 = 4;
+    end1 = 3;
+    tvnj::blockSwap(array, start1, end1, start2);
+    UNIT_TEST_EQ(tvnj::join(array, ","), "5,6,7,8,1,2,3,4,9");
 }
