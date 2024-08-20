95ac02964b25bb249f73ab30b29a7a653cac72c0
TelevisionNinja <TelevisionNinja@gmail.com>
2024-08-19 19:46:22 -0500

    added more heap tests

--- tests/heapTests.h
+++ tests/heapTests.h
@@ -1,17 +1,35 @@
 #include "../src/unitTest.h"
 #include "../src/data structures/MaxBinaryHeap.h"
+#include "../src/data structures/MinBinaryHeap.h"
 
 void maxHeapTests() {
     tvnj::MaxBinaryHeap<int> heapA({1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17});
     UNIT_TEST_EQ(heapA.toString(), "[17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1]");
+    UNIT_TEST_NEQ(heapA.toString(), "[1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]");
 
     tvnj::MaxBinaryHeap<int> heapB({10, 5, 3, 2, 4});
     heapB.insert(15);
     UNIT_TEST_EQ(heapB.toString(), "[15, 5, 10, 2, 4, 3]");
+    UNIT_TEST_NEQ(heapB.toString(), "[10, 5, 3, 2, 4, 15]");
 
     tvnj::MaxBinaryHeap<int> heapC({ 10, 5, 3, 2, 4});
     int popped = 0;
     UNIT_TEST_EQ(heapC.remove(popped), true);
+    UNIT_TEST_NEQ(heapC.toString(), "[10, 5, 3, 2, 4]");
     UNIT_TEST_EQ(popped, 10);
     UNIT_TEST_EQ(heapC.toString(), "[5, 4, 3, 2]");
+
+    tvnj::MinBinaryHeap<int> heapAmin({1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17});
+    UNIT_TEST_NEQ(heapAmin.toString(), "[17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1]");
+    UNIT_TEST_EQ(heapAmin.toString(), "[1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]");
+
+    tvnj::MinBinaryHeap<int> heapBmin({13, 16, 31, 41, 51, 100});
+    UNIT_TEST_EQ(heapBmin.toString(), "[13, 16, 31, 41, 51, 100]");
+    UNIT_TEST_NEQ(heapBmin.toString(), "[100, 51, 41, 31, 16, 13]");
+
+    popped = 0;
+    UNIT_TEST_EQ(heapBmin.remove(popped), true);
+    UNIT_TEST_NEQ(heapBmin.toString(), "[13, 16, 31, 41, 51, 100]");
+    UNIT_TEST_EQ(popped, 13);
+    UNIT_TEST_EQ(heapBmin.toString(), "[16, 41, 31, 100, 51]");
 }
