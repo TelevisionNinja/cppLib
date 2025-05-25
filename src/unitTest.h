dd06533016d708278fd85eb046b9f2d9514e5011
TelevisionNinja <TelevisionNinja@gmail.com>
2025-05-25 11:55:12 -0400

    fix not equals unit test output

--- src/unitTest.h
+++ src/unitTest.h
@@ -10,6 +10,6 @@
 
 #include <iostream>
 
-#define UNIT_TEST_NEQ(value, expected) { if (value == expected) { std::cout << __FUNCTION__ << "() failed UNIT_TEST_NEQ at line: " << __LINE__ << "\nValue:\n" << value << "\n\nExpected:\n" << expected << "\n\n"; } }
+#define UNIT_TEST_NEQ(value, notExpected) { if (value == notExpected) { std::cout << __FUNCTION__ << "() failed UNIT_TEST_NEQ at line: " << __LINE__ << "\nValue:\n" << value << "\n\nNot Expected:\n" << notExpected << "\n\n"; } }
 
 #endif
